4db604d016f193851bb08da71149192b91b7768a
feiy <feiy@nvidia.com>
2020-06-10 16:16:22 +0800

    Setting version number to 0.1.1

--- python/setup.py
+++ python/setup.py
@@ -3,7 +3,7 @@ from codecs import open
 
 setup(
 	name = 'VkInline',
-	version = '0.1.0',
+	version = '0.1.1',
 	description = 'A tool making it easy to use Vulkan for Python',
 	url='https://github.com/fynv/VkInline',
 	license='Anti 996',
