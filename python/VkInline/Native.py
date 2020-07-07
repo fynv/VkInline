00bd4801f0d190bb3d634a213410639a9194ba77
feiy <feiy@nvidia.com>
2020-07-07 19:15:37 +0800

    bug fix (vulkan version selection)

--- python/VkInline/Native.py
+++ python/VkInline/Native.py
@@ -1,14 +1,17 @@
-import os
-import sys
-import site
-from .cffi import ffi
+from .NativeEX import native
+if native.n_vkinline_try_init()==0:
+	import os
+	import sys
+	import site
+	from .cffi import ffi
 
-if os.name == 'nt':
-    fn_vkinline = 'PyVkInline.dll'
-elif os.name == "posix":
-    fn_vkinline = 'libPyVkInline.so'
+	if os.name == 'nt':
+	    fn_vkinline = 'PyVkInline.dll'
+	elif os.name == "posix":
+	    fn_vkinline = 'libPyVkInline.so'
 
-path_vkinline = os.path.dirname(__file__)+"/"+fn_vkinline
-
-native = ffi.dlopen(path_vkinline)
+	path_vkinline = os.path.dirname(__file__)+"/"+fn_vkinline
+	native = ffi.dlopen(path_vkinline)
+else:
+	from .NativeEX import ffi
 
--- python/VkInline/__init__.py
+++ python/VkInline/__init__.py
@@ -1,8 +1,6 @@
-from .NativeEX import native
+from .Native import native
 if native.n_vkinline_try_init()==0:
-	from .Native import native
-	if native.n_vkinline_try_init()==0:
-		raise ImportError('cannot import VkInline')
+	raise ImportError('cannot import VkInline')
 
 from .Context import *
 from .ShaderViewable import *
